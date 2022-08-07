e57ed296502705e092c40bc81be9bfdc650da644
Yahia-2010 <yahiaberash8@gmail.com>
2022-08-08 01:12:54 +0300

    finishing Navbar stories

--- frontend/src/layout/Navbar/Navbar.stories.tsx
+++ frontend/src/layout/Navbar/Navbar.stories.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Navbar from "./Navbar";
+import { BrowserRouter } from "react-router-dom";
 
 export default {
   title: "Navbar",
   component: Navbar,
 } as ComponentMeta<typeof Navbar>;
 
-const Template: ComponentStory<typeof Navbar> = () => <Navbar />;
+const Template: ComponentStory<typeof Navbar> = () => (
+  <BrowserRouter>
+    <Navbar />
+  </BrowserRouter>
+);
 
 export const Default = Template.bind({});
-
-export const Dark = Template.bind({});
-
-export const Small = Template.bind({});
