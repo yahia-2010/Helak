77cfe93725607bbf700628250e0cc8366e8d1493
Yahia-2010 <yahiaberash8@gmail.com>
2022-08-10 17:25:04 +0300

    adding show password functionality to Input

--- frontend/src/components/forms/Input/Input.tsx
+++ frontend/src/components/forms/Input/Input.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
+import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
 
 export interface InputProps {
   label?: string;
@@ -12,18 +13,36 @@ export interface InputProps {
 
 const Input: React.FC<InputProps> = (props) => {
   const { label, type, onChange, value, error } = props;
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <div className="flex flex-col gap-y-1">
       {label && <label className="text-[.85rem]">{label}</label>}
-      <input
-        {...props}
-        type={type}
-        onChange={onChange}
-        value={value}
-        className={`${(props as any).className} disabled:opacity-75 ${
-          error && "bg-red-100"
-        } rounded border-0 bg-gray-100 px-2 py-1 text-[.85rem] dark:bg-dark-secondary`}
-      />
+      <div className="relative">
+        <input
+          {...props}
+          type={
+            type === "password" ? (passwordVisible ? "text" : "password") : type
+          }
+          onChange={onChange}
+          value={value}
+          className={`${(props as any).className} w-full disabled:opacity-75 ${
+            error && "bg-red-100"
+          } rounded border-0 bg-gray-100 px-2 py-1 text-[.85rem] dark:bg-dark-secondary`}
+        />
+        {type === "password" && (
+          <button
+            className="absolute left-2.5 top-1/2 -translate-y-1/2"
+            onClick={() => setPasswordVisible(!passwordVisible)}
+          >
+            {!passwordVisible ? (
+              <EyeOffIcon className="h-4" />
+            ) : (
+              <EyeIcon className="h-4" />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
